f84f3d75fc87888bbef79b4fa8c834e8bb77dce6
Andrey Zakharov <mediaexplorer74@hotmail.com>
2023-11-08 03:03:10 +0300

    .

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
 # GeoWar
-A Geometry Wars clone to learn the MonoGame framework and C# in general following the tutorial: 
 
+
+## Screenshot(s) (Target: win 11 on desktop pc, not W10M yet)
+![Button](Images/Q01.png)
+
+## About
 https://gamedevelopment.tutsplus.com/tutorials/make-a-neon-vector-shooter-in-xna-basic-gameplay--gamedev-9859
 
+
+## How to create this UWP XNA game?
 Follow the Something Random youtube channel for development updates: 
 
 https://www.youtube.com/channel/UCdbQJRNyC7JdgvsxZLNhCzw
+
+## Status of W10M port
+- UWP base (sdk 10240) - ready
+- Music (song) play - broken
+- Game speed -- crazy =(
+- No touch sense yet, only original kbd control at now
+
+## .
+As is. Learn purposes only
+
+## ..
+[m][e] 2023
---
+++ Src/GeoWar.UWP/Game/Art.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+
+namespace GeoWar
+{
+    /// <summary>
+    /// A class for loading and storing all the textures in the game
+    /// like the player, enemies and bullets. a static class cannot and
+    /// does not need to be instantiated before execution.
+    /// </summary>
+    class Art
+    {
+        private static Texture2D _player;
+        private static Texture2D _seeker;
+        private static Texture2D _wanderer;
+        private static Texture2D _bullet;
+        private static Texture2D _pointer;
+        private static SpriteFont _font;
+
+        /// <summary>
+        /// A helpful and tidy load method to load all the textures from a one line
+        /// Art.Load() in the LoadContent() of GameRoot
+        /// </summary>
+        /// <param name="game"></param>
+        public static void Load(GameRoot instance)
+        {
+            _player = instance.Content.Load<Texture2D>("Art\\Player");
+            _seeker = instance.Content.Load<Texture2D>("Art\\Seeker");
+            _wanderer = instance.Content.Load<Texture2D>("Art\\Wanderer");
+            _bullet = instance.Content.Load<Texture2D>("Art\\Bullet");
+            _pointer = instance.Content.Load<Texture2D>("Art\\Pointer");
+            _font = instance.Content.Load<SpriteFont>("Font");
+        }
+
+        public static Texture2D Player
+        {
+            get { return _player; }
+        }
+
+        public static Texture2D Seeker
+        {
+            get { return _seeker; }
+        }
+
+        public static Texture2D Wanderer
+        {
+            get { return _wanderer; }
+        }
+
+        public static Texture2D Bullet
+        {
+            get { return _bullet; }
+        }
+
+        public static Texture2D Pointer
+        {
+            get { return _pointer; }
+        }
+
+        public static SpriteFont Font
+        {
+            get { return _font; }
+        }
+    }
+}
